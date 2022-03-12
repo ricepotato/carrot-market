988c53c1d58d3a3f6a237ad4102f32869b3ed163
ricepotato <sukjun40@naver.com>
2022-03-12 19:16:50 +0900

    feat: import paths

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,11 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "../../../libs/server/withHandler";
+import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  //res.status(200).end();
   return res.json({ ok: true });
-  //res.json({ ok: true, data: "xx" });
 }
 
 export default withHandler("POST", handler);
