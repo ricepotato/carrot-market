17f37194af7623b3e1ce46a433dcf9cc68ccd41e
ricepotato <sukjun40@naver.com>
2022-03-16 02:19:39 +0900

    feat: items -> products

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
             hearts={1}
           />
         ))}
-        <FloatingButton href="/items/upload">
+        <FloatingButton href="/producs/upload">
           <svg
             className="h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
