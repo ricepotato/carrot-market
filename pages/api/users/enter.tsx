e7db3153dffbdb98f23ebb466c5793e562053a39
ricepotato <sukjun40@naver.com>
2022-03-16 01:34:21 +0900

    feat: protected handler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -34,4 +34,4 @@ async function handler(
   return res.json({ ok: true, token: token.payload });
 }
 
-export default withHandler("POST", handler);
+export default withHandler({ method: "POST", handler, isPrivate: false });
--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -21,4 +21,4 @@ async function handler(
   });
 }
 
-export default withApiSession(withHandler("GET", handler));
+export default withApiSession(withHandler({ method: "GET", handler }));
